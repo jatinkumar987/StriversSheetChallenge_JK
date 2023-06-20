6c81fa5aeebbd294cb2091e7738d47d3398c4cf5
JATIN KUMAR <76620715+jatinkumar987@users.noreply.github.com>
2023-06-20 17:15:45 +0000

    best time to buy and sale stock

---
+++ Stock_buy_sale.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+
+   int maxpro=0;
+   int minprice=INT_MAX;
+
+   for(int i=0;i<prices.size();i++){
+        minprice=min(minprice,prices[i]);
+        maxpro=max(maxpro,prices[i]-minprice);
+
+   }
+   return maxpro;
+
+}
